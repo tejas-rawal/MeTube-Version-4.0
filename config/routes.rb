e8e812a1f173c1d91a3d5ed35d3a4c5db6afacd0
Tejas Rawal <trawal90@gmail.com>
2013-12-04 10:20:43 -0600

    Add #new to create form. Add route to #new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ MetubeCohort3::Application.routes.draw do
 
   root 'videos#show_all'
   get '/videos' => 'videos#show_all'
+  get '/videos/new' => 'videos#new', as: 'new_video'
   get '/videos/:id' => 'videos#show', as: 'video'
 
 end
