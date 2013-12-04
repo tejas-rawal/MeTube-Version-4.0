5bea85c4fe8ec1b6b35cd0c935a0dba7c389a567
Tejas Rawal <trawal90@gmail.com>
2013-12-04 10:47:04 -0600

    Add get request to edit and put and patch request to #update so we can change attributes in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ MetubeCohort3::Application.routes.draw do
   get '/videos' => 'videos#show_all'
   get '/videos/new' => 'videos#new', as: 'new_video'
   get '/videos/:id' => 'videos#show', as: 'video'
+  get '/videos/:id/edit' => 'videos#edit', as: 'edit_video'
   post '/videos' => 'videos#create'
+  put '/videos/:id' => 'videos#update'
+  patch '/videos/:id' => 'videos#update'
 
 end
